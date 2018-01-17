97cd0c83cc4eae545ba017159043d84da68eb5ad
Leonardo Cordoba - Cientifico Datos <vsilecor@lapp-prpm066.visa2.com.ar>
2018-01-17 14:20:24 -0300

    Cambios menores

--- R/importar_archivo.R
+++ R/importar_archivo.R
@@ -1,4 +1,8 @@
-# Importar el archivo
+# Este script importa el archivo para predecir el mes corriente y lo escribe
+rm(list = ls())
+gc()
+
+# Importar archivo --------------------------------------------------------
 
 library(dplyr)
 library(data.table)
@@ -6,7 +10,14 @@ library(data.table)
 source("/home/Compartida_CD_GI/d_visa_scoring/scripts/basicos.R")
 source("/home/Compartida_CD_GI/d_visa_scoring/scripts/funciones.R")
 
-query<- "select * from d_cientificos_datos.vs_input_modelo_v"
+args <- commandArgs(trailingOnly=TRUE)
+cod_banco <- args[1]
+cod_mes <- args[2]
+
+query<- paste0("select * from d_cientificos_datos.vs_input_modelo_v where cod_banco = ", cod_banco, " and cod_mes = ", cod_mes)
 
 input_modelo <- get_query(query = query , "TD")
 
+# Escribir archivo --------------------------------------------------------
+
+fwrite(input_modelo, paste0(dir_info_actual,"input_modelo_",cod_banco,"_",cod_mes,".csv"))
