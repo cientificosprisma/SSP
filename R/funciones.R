103246cb05afd5fc7549b057cb93838c048a454e
Leonardo Cordoba - Cientifico Datos <vsilecor@lapp-prpm066.visa2.com.ar>
2018-01-17 17:13:48 -0300

    Armo funciones para mapear factores

--- R/funciones.R
+++ R/funciones.R
@@ -34,3 +34,79 @@ get_query <- function(query, db){
   return(result)
   
 }
+
+
+# Funciones para guardar mapeo y luego mapear factores ----------------------------------------------
+save_mapping <- function(df){
+  col_idx <- c()
+  
+  for (i in 1:ncol(df)){
+    
+    if (typeof(input_modelo[,i])=="character"){
+      col_idx <- c(col_idx,i)
+    }
+  }
+  
+  for (i in col_idx){
+    assign(paste0("col_key_",i),sort(unique(input_modelo[,i])))
+    len <- length(get(paste0("col_key_",i)))
+    vector <- 1:len
+    key_value <- data.frame(key = get(paste0("col_key_",i)), value = vector)
+    col_name <- colnames(df)[i]
+    colnames(key_value)[1] <- col_name
+    saveRDS(key_value, paste0(dir_configuracion_columnas, col_name,".RDS"))
+    }
+}
+
+map_factors <- function(df){
+  library(dplyr)
+  col_idx <- c()
+  
+  for (i in 1:ncol(df)){
+    
+    if (typeof(input_modelo[,i])=="character"){
+      col_idx <- c(col_idx,i)
+    }
+  }
+  
+  for (i in col_idx){
+    col_name <- colnames(df)[i]
+    if(file.exists(paste0(dir_configuracion_columnas, col_name,".RDS"))){
+      assign(paste0(col_name,"_value"), readRDS(paste0(dir_configuracion_columnas, col_name,".RDS")))
+      suppressMessages(df <- df %>% left_join(y = get(paste0(col_name,"_value"))))
+      df[,col_name] <- df[,"value"]
+      df$value <- NULL
+} else{
+      df[,i] <- 9999
+    }
+  }
+  return(df)
+}
+
+# Funcion que genera la matriz para xgboost -------------------------------
+
+## Importa mp_entrenar y devuelve un df preparado para xgboost, para pasarselo al task de MLR
+
+get_xgboost_matrix <- function(df, clase){
+
+  # partition es == 1 si se splitea en train y test y partition == 2 si se splitea en train, test y calibration
+    
+    library(data.table)
+    library(stringr)
+    dt <- as.data.table(df)
+    
+    ## Me quedo con los is.na(clase) == FALSE
+    
+    dt <- dt[is.na(get(clase)) == FALSE]
+    
+    colnames(dt) <- str_replace(colnames(df),"\\$","__")
+    
+    #Hay dos columnas que falta acomodar
+    
+    dtMatrix <- dt[,lapply(.SD,as.numeric)] %>% as.matrix
+    
+    dt <- dt %>% mutate(clase = as.factor(get(clase)))
+    
+    return(dt)
+
+}
