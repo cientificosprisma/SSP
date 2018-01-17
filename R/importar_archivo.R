663b86c4e645ba13ef7ef8ad54746551b3f82eac
Guillermo Prinzen - Cientifico Datos <vsigupri@lapp-prpm066.visa2.com.ar>
2018-01-17 13:53:51 -0300

    Se crea el archivo

--- R/importar_archivo.R
+++ R/importar_archivo.R
@@ -1,5 +1,3 @@
-# Este script sirve para levantar la historia o escribir
-
 rm(list = ls())
 gc()
 
@@ -11,8 +9,12 @@ library(data.table)
 source("/home/Compartida_CD_GI/d_visa_scoring/scripts/basicos.R")
 source("/home/Compartida_CD_GI/d_visa_scoring/scripts/funciones.R")
 
-query<- "select * from d_cientificos_datos.vs_input_modelo_v"
+args <- commandArgs()
+cod_banco <- args[1]
+cod_mes <- args[2]
+
+query<- paste0("select * from d_cientificos_datos.vs_input_modelo_v where cod_banco = ", cod_banco, "and cod_mes = ", cod_mes)
 
 input_modelo <- get_query(query = query , "TD")
 
-fwrite(input_modelo, paste0(dir_info_historia,"input_modelo_",year,"_",month))
+fwrite(input_modelo, paste0(dir_modelos_actual,"input_modelo_",cod_banco,"_",cod_mes))
