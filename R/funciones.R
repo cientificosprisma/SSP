e40f6e33a90fa0f3d34e87d7a7f79fe5457ee1d7
Guillermo Prinzen - Cientifico Datos <vsigupri@lapp-prpm066.visa2.com.ar>
2018-01-24 10:51:02 -0300

    agrego funcion auc

--- R/funciones.R
+++ R/funciones.R
@@ -289,3 +289,14 @@ add_na_elem<-function(a){
  return( c(a,"__NAs__"))
   
 }
+
+# Funcion que calcula el AUC dadas las proba y clase
+fastAUC <- function(probs, class) {
+  x <- probs
+  y <- class
+  x1 = x[y==1]; n1 = length(x1); 
+  x2 = x[y==0]; n2 = length(x2);
+  r = rank(c(x1,x2))  
+  auc = (sum(r[1:n1]) - n1*(n1+1)/2) / n1 / n2
+  return(auc)
+}
