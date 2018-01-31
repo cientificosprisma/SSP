5f327afa7cec2f6d193b63906b35e91245a325b3
Matias Monja - Cientifico Datos <vsimamon@lapp-prpm066.visa2.com.ar>
2018-01-31 09:27:29 -0300

    corrijo el merge del ranger

--- R/entrenar_randomforest.R
+++ R/entrenar_randomforest.R
@@ -14,7 +14,6 @@ library(mlr)
 library(dplyr)
 library(mlrMBO)
 library(ranger)
-library(data.table)
 
 set.seed(12112016)
 
@@ -22,13 +21,14 @@ source("/home/Compartida_CD_GI/d_visa_scoring/scripts/basicos.R")
 source("/home/Compartida_CD_GI/d_visa_scoring/scripts/funciones.R")
 
 # Importamos archivo ------------------------------------------------------
-df <- read_info("input_modelo_historia_g1.csv","historia", 2000000,strings_as_factors=FALSE)
+
+
+df <- read_info("input_modelo_2018_1.csv","historia", 1000,strings_as_factors=FALSE)
+
 df <- df %>% select(-c(clase_cp_1, clase_cp_2, clase_lp_1, clase_lp_2))
 df <- df %>% rename(clase = clase_veraz)
 df[is.na(df)]<-(-99999)
 df[,"Cod_Mes"]<-as.integer(df[,"Cod_Mes"])
-
-
 
 ###### CREAR FUNCION #####
 library(data.table)
@@ -45,11 +45,7 @@ for(i in 1:ncol(na_row)){if(typeof(na_row[,i])!="character"){na_row[,i]<-(-99999
 for(i in 1:ncol(na_row)){if(typeof(na_row[,i])=="character"){na_row[,i]<-("__NAs__")}}
 df<-rbind(df,na_row)
 
-factores_nominales<-as.list(fread(paste0(dir_configuracion_columnas,"factores_nominales.csv"),header = FALSE))
-factores_ordinales<-as.list(fread(paste0(dir_configuracion_columnas,"factores_ordinales.csv"),header = FALSE))
-factores<-as.list(c(factores_nominales,factores_ordinales))
-
-actualiza_lks(df,factores)
+actualiza_lks(df)
 #######
 
 # Partimos en train y test ------------------------------------------------
@@ -72,23 +68,23 @@ gc()
 
 
 
-############## Se cambian los tipo de datos que corresponden a factores
+############## Lookups y Mapeos
+
+df<-lookupea_factores(df)
+for(c in 1:ncol(df)){
+  if(typeof(df[,c])=="character"){df[,c]<-as.factor(df[,c])}
+  
+}
+df[,"clase"]<-as.factor(df[,"clase"])
 
 
 
 
-df<-a_factor(df,lista_col =factores_nominales ,orden = FALSE)
-df<-a_factor(df,lista_col =factores_ordinales ,orden = TRUE)
 
 
+##############
 
-
-
-# BUSQUEDA DE MEJORES HIPERPARAMETROS -------------------------------------
-
-
-
-  ## Task, Learner, metodo de sampleo --------------------------------------------------------------
+# Task, Learner, metodo de sampleo --------------------------------------------------------------
 
 #df$Cod_Tipo_Cuenta <- as.factor(as.character(df$Cod_Tipo_Cuenta))
 task = makeClassifTask(data = df, target = "clase", fixup.data = "no")
@@ -103,7 +99,9 @@ sampling_method = makeFixedHoldoutInstance(train_inds, test_inds, nrow(df))
 
 randomforest_learner <- makeLearner("classif.ranger", predict.type = "prob",fix.factors.prediction = TRUE)
 
+
 randomforest_learner$par.vals<- list(num.threads =4 )
+
 
 low_mtry<-round(ncol(df)*0.2)
 upp_mtry<-round(ncol(df)*0.9)
@@ -122,15 +120,12 @@ des = generateDesign(n = 15, par.set = randomforest_params)
 # Extends an MBO control object with infill criteria and infill optimizer options
 mbo_control <- setMBOControlTermination(mbo_control, iters = 35)
 
+
 # Defining surrogate learner
 surrogate_lrn <- makeLearner("regr.km", predict.type = "se")
-clase = df[train_inds,"clase"]
-nuggets = 1e-8*var(clase)
-
-surrogate_lrn <- setHyperPars(learner = surrogate_lrn, par.vals = list(nugget=nuggets))
 
 # Create control object for hyperparameter tuning with MBO
-tune_control = mlr:::makeTuneControlMBO(learner = surrogate_lrn, mbo.control = mbo_control, mbo.design = des)
+tune_control = mlr:::makeTuneControlMBO(learner = surrogate_lrn, mbo.control = mbo_control)
 
 # Tuneo parametros --------------------------------------------------------
 rf_tune <-
@@ -151,24 +146,52 @@ cod_mes<-max(df$Cod_Mes)
 
 write.csv(opt_results, paste0(dir_performance_modelos_pruebas,"randomforest_", cod_mes,".csv"), row.names = FALSE)
 
-
-
 # Elijo los mejores parametros y entreno--------------------------------------------
 
 ##### Completo clase train
+dif_sexo<-c(sexo=levels(df$Cod_Sexo_Host)[!( levels(df$Cod_Sexo_Host)%in% unique(train$Cod_Sexo_Host)  )])
+dif_sexo<-add_na_elem(dif_sexo)
+#colnames(dif_sexo)<-"sexo"
+dif_Segmento_Credito<-c( levels(df$Segmento_Credito)[!( levels(df$Segmento_Credito) %in% unique(train$Segmento_Credito) )])
+dif_Segmento_Credito<-add_na_elem(dif_Segmento_Credito)
+#colnames(dif_Segmento_Credito)<-"Seg_Credito"
+dif_Cod_Tipo_Tarjeta<-c(levels(df$Cod_Tipo_Tarjeta)[!(  levels(df$Cod_Tipo_Tarjeta) %in% unique(train$Cod_Tipo_Tarjeta))])
+dif_Cod_Tipo_Tarjeta<-add_na_elem(dif_Cod_Tipo_Tarjeta)
+#colnames(dif_Cod_Tipo_Tarjeta)<-"Tipo_Tarjeta"
+dif_Cod_Region_Geografica_Host<-c(v1=levels(df$Cod_Region_Geografica_Host)[!(  levels(df$Cod_Region_Geografica_Host) %in% unique(train$Cod_Region_Geografica_Host))])
+dif_Cod_Region_Geografica_Host<-add_na_elem(dif_Cod_Region_Geografica_Host)
+#colnames(dif_Cod_Region_Geografica_Host)<-"Region_Geografica"
+dif_Cod_Limite_Compra<-c(v1=levels(df$Cod_Limite_Compra)[!(  levels(df$Cod_Limite_Compra) %in% unique(train$Cod_Limite_Compra))])
+dif_Cod_Limite_Compra<-add_na_elem(dif_Cod_Limite_Compra)
+#colnames(dif_Cod_Limite_Compra)<-"Cod_Limite"
+dif_Cod_Tipo_Identificacion_Host<-c(v1=levels(df$Cod_Tipo_Identificacion_Host)[!(  levels(df$Cod_Tipo_Identificacion_Host) %in% unique(train$Cod_Tipo_Identificacion_Host))])
+dif_Cod_Tipo_Identificacion_Host<-add_na_elem(dif_Cod_Tipo_Identificacion_Host)
+#colnames(dif_Cod_Tipo_Identificacion_Host)<-"Tipo_Identificacion"
+dif_Cod_Tipo_Cuenta<-c(v1=levels(df$Cod_Tipo_Cuenta)[!(  levels(df$Cod_Tipo_Cuenta) %in% unique(train$Cod_Tipo_Cuenta))])
+dif_Cod_Tipo_Cuenta<-add_na_elem(dif_Cod_Tipo_Cuenta)
+#colnames(dif_Cod_Tipo_Cuenta)<-"Tipo_Cuenta"
 
+library(data.table)
+faltantes_train<-data.table(na_row,dif_Segmento_Credito,dif_Cod_Region_Geografica_Host,dif_Cod_Tipo_Tarjeta,dif_sexo,dif_Cod_Limite_Compra,dif_Cod_Tipo_Identificacion_Host,dif_Cod_Tipo_Cuenta,keep.rownames = TRUE)   
+faltantes_train<-as.data.frame(faltantes_train)
+faltantes_train<-faltantes_train %>% mutate(Cod_Sexo_Host=dif_sexo,Segmento_Credito=dif_Segmento_Credito,Cod_Tipo_Tarjeta=dif_Cod_Tipo_Tarjeta,Cod_Region_Geografica_Host=dif_Cod_Region_Geografica_Host, Cod_Limite_Compra=dif_Cod_Limite_Compra,Cod_Tipo_Identificacion_Host=dif_Cod_Tipo_Identificacion_Host,Cod_Tipo_Cuenta=dif_Cod_Tipo_Cuenta,clase=0) %>% 
+  select(-c(dif_Cod_Tipo_Cuenta,dif_Cod_Region_Geografica_Host,dif_sexo,dif_Cod_Tipo_Identificacion_Host,dif_Segmento_Credito,dif_Cod_Tipo_Tarjeta,dif_Cod_Limite_Compra,rn))
 
-faltantes_train<-obtener_factores_faltantes(train,df,factores_nominales)
+for(i in 1:ncol(na_row)){if(class(faltantes_train[,i])!="character" & colnames(faltantes_train[,i])!="clase"){faltantes_train[,i]<-(-99999)}}
+
 train<-rbind(train,faltantes_train)
 
-train<-lookupea_factores(train,factores)
-test<-lookupea_factores(test,factores)
+train<-lookupea_factores(train)
+test<-lookupea_factores(test)
 
 ####
+unique(test$clase)
 
-
-train<-a_factor(train,lista_col =factores_nominales ,orden = FALSE)
-train<-a_factor(train,lista_col =factores_ordinales ,orden = TRUE)
+for(c in 1:ncol(train)){
+  if(typeof(train[,c])=="character"){train[,c]<-as.factor(train[,c])}
+  
+}
+train[,"clase"]<-as.factor(train[,"clase"])
 
 #ver hyperparametros probados y su resultado
 #View(as.data.frame(opt.state$opt.path))
@@ -183,8 +206,11 @@ rf_model <- train(rf_new, train_task)
 
 # Predigo test ----------------------------------------------
 
-test<-a_factor(test,lista_col =factores_nominales ,orden = FALSE)
-test<-a_factor(test,lista_col =factores_ordinales ,orden = TRUE)
+for(c in 1:ncol(test)){
+  if(typeof(test[,c])=="character"){test[,c]<-as.factor(test[,c])}
+  
+}
+test[,"clase"]<-as.factor(test[,"clase"])
 
 #levels(df$Cod_Tipo_Cuenta)
 #levels(train$Cod_Tipo_Cuenta)
