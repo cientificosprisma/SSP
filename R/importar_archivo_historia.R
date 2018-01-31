a246dac9228bc20d1bb31abaef2a78b46545c983
Leonardo Cordoba - Cientifico Datos <vsilecor@lapp-prpm066.visa2.com.ar>
2018-01-31 11:16:06 -0300

    cambios

--- R/importar_archivo_historia.R
+++ R/importar_archivo_historia.R
@@ -11,17 +11,7 @@ library(data.table)
 source("/home/Compartida_CD_GI/d_visa_scoring/scripts/basicos.R")
 source("/home/Compartida_CD_GI/d_visa_scoring/scripts/funciones.R")
 
-<<<<<<< HEAD
 query<- paste0("select * from d_cientificos_datos.vs_input_modelo_hist_v where tipo_clase=1")
-=======
-args <- commandArgs(trailingOnly=TRUE)
-cod_banco <- args[1]
-cod_mes <- args[2]
-
-print(args)
-
-query<- paste0("select * from d_cientificos_datos.vs_input_modelo_v")
->>>>>>> 5f327afa7cec2f6d193b63906b35e91245a325b3
 
 input_modelo <- get_query(query = query , "TD")
 
